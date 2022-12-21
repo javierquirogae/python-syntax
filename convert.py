b3dbecf1c829020f7d859df3f75e7ebdbcacdd78
Javier Quiroga <javier.quiroga.e@gmail.com>
2022-12-21 18:36:35 -0500

    pat 1 complete

--- convert.py
+++ convert.py
@@ -1,5 +1,5 @@
 def convert_temp(unit_in, unit_out, temp):
-    """Convert farenheit <-> celsius and return results.
+	"""Convert farenheit <-> celsius and return results.
 
     - unit_in: either "f" or "c" 
     - unit_out: either "f" or "c"
@@ -11,16 +11,34 @@ def convert_temp(unit_in, unit_out, temp):
 
     For example:
 
-      convert_temp("c", "f", 0)  =>  32.0
-      convert_temp("f", "c", 212) => 100.0
+		convert_temp("c", "f", 0)  =>  32.0
+		convert_temp("f", "c", 212) => 100.0
     """
-
+	input_units = [unit_out,unit_in]
+	invalid_unit = None
     # YOUR CODE HERE
+	if (unit_in == 'c' or unit_in == 'f') and (unit_out == 'c' or unit_out == 'f'):
+		if unit_in == unit_out:
+			return temp
+		elif unit_in == 'f' and unit_out == 'c':
+			return (temp - 32)*5/9
+		elif unit_in == 'c' and unit_out == 'f':
+			return (temp*9/5) + 32
+	else:
+		for u in input_units:
+			if u != 'c' and u != 'f':
+				invalid_unit = u
+		return f"Invalid unit {invalid_unit}"
 
 
-print("c", "f", 0, convert_temp("c", "f", 0), "should be 32.0")
-print("f", "c", 212, convert_temp("f", "c", 212), "should be 100.0")
-print("z", "f", 32, convert_temp("z", "f", 32), "should be Invalid unit z")
-print("c", "z", 32, convert_temp("c", "z", 32), "should be Invalid unit z")
-print("f", "f", 75.5, convert_temp("f", "f", 75.5), "should be 75.5")
+    
 
+
+
+print("c --> f", "0 -->", convert_temp("c", "f", 0), "should be 32.0")
+print("f --> c", "212 -->", convert_temp("f", "c", 212), "should be 100.0")
+print("z --> f", "32 -->", convert_temp("z", "f", 32), "should be Invalid unit z")
+print("c --> z", "32 -->", convert_temp("c", "z", 32), "should be Invalid unit z")
+print("f --> f", "75.5 -->", convert_temp("f", "f", 75.5), "should be 75.5")
+
+ 
